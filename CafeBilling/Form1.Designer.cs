bfdf29962b402bd3db8334391307402a881f4c18
besagasandrei-code <besagasandrei@gmail.com>
2026-09-07 14:35:32 +0800

    update

--- CafeBilling/Form1.Designer.cs
+++ CafeBilling/Form1.Designer.cs
@@ -28,13 +28,212 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Burger",
+            "P85"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Chicken Sandwich",
+            "P75"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Spaghetti",
+            "P95"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Fries",
+            "P50"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem(new string[] {
+            "SoftDrink",
+            "P35"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Iced Tea",
+            "P40"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem7 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Coffee",
+            "P45"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem8 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Bottled Water",
+            "P25"}, -1);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.FoodBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ReceiptMultiBox = new System.Windows.Forms.TextBox();
+            this.DrinksBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.FoodQuantiBox = new System.Windows.Forms.TextBox();
+            this.DrinksQuantiBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.HideSelection = false;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4});
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(171, 112);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Foods";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Price";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader2.Width = 58;
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView2.HideSelection = false;
+            this.listView2.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem5,
+            listViewItem6,
+            listViewItem7,
+            listViewItem8});
+            this.listView2.Location = new System.Drawing.Point(12, 130);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(171, 112);
+            this.listView2.TabIndex = 1;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Drinks";
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Price";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // FoodBox
+            // 
+            this.FoodBox.Location = new System.Drawing.Point(243, 72);
+            this.FoodBox.Name = "FoodBox";
+            this.FoodBox.Size = new System.Drawing.Size(119, 20);
+            this.FoodBox.TabIndex = 2;
+            this.FoodBox.TextChanged += new System.EventHandler(this.FoodBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(239, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Order";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ReceiptMultiBox
+            // 
+            this.ReceiptMultiBox.Location = new System.Drawing.Point(617, 12);
+            this.ReceiptMultiBox.Multiline = true;
+            this.ReceiptMultiBox.Name = "ReceiptMultiBox";
+            this.ReceiptMultiBox.Size = new System.Drawing.Size(171, 282);
+            this.ReceiptMultiBox.TabIndex = 4;
+            this.ReceiptMultiBox.TextChanged += new System.EventHandler(this.ReceiptMultiBox_TextChanged);
+            // 
+            // DrinksBox
+            // 
+            this.DrinksBox.Location = new System.Drawing.Point(243, 130);
+            this.DrinksBox.Name = "DrinksBox";
+            this.DrinksBox.Size = new System.Drawing.Size(119, 20);
+            this.DrinksBox.TabIndex = 5;
+            this.DrinksBox.TextChanged += new System.EventHandler(this.DrinksBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(276, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Foods";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(276, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Drinks";
+            // 
+            // FoodQuantiBox
+            // 
+            this.FoodQuantiBox.Location = new System.Drawing.Point(444, 72);
+            this.FoodQuantiBox.Name = "FoodQuantiBox";
+            this.FoodQuantiBox.Size = new System.Drawing.Size(42, 20);
+            this.FoodQuantiBox.TabIndex = 9;
+            // 
+            // DrinksQuantiBox
+            // 
+            this.DrinksQuantiBox.Location = new System.Drawing.Point(444, 130);
+            this.DrinksQuantiBox.Name = "DrinksQuantiBox";
+            this.DrinksQuantiBox.Size = new System.Drawing.Size(42, 20);
+            this.DrinksQuantiBox.TabIndex = 10;
+            this.DrinksQuantiBox.TextChanged += new System.EventHandler(this.DrinksQuantiBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DrinksQuantiBox);
+            this.Controls.Add(this.FoodQuantiBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.DrinksBox);
+            this.Controls.Add(this.ReceiptMultiBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FoodBox);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.TextBox FoodBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ReceiptMultiBox;
+        private System.Windows.Forms.TextBox DrinksBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox FoodQuantiBox;
+        private System.Windows.Forms.TextBox DrinksQuantiBox;
     }
 }
 
--- CafeBilling/Form1.cs
+++ CafeBilling/Form1.cs
@@ -16,5 +16,30 @@ namespace CafeBilling
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FoodBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DrinksBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ReceiptMultiBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DrinksQuantiBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
